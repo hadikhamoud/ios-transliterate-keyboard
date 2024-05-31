1ced2a47ea301a68f167ac61b9f3c2d898781358
Hadi Hamoud <hkh21@mail.aub.edu>
2024-05-31 17:39:50 +0300

    Initial Commit

---
+++ transliterate-keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  transliterate-keyboard
+//
+//  Created by Hadi Hamoud on 5/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ transliterate-keyboard/transliterate_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  transliterate_keyboardApp.swift
+//  transliterate-keyboard
+//
+//  Created by Hadi Hamoud on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct transliterate_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
